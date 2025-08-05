f7ce98768824ff28575401a40c996163728c5721
rudini <roger.rudin@gmail.com>
2025-08-05 21:04:22 +0000

    Deploying to prod from @ hftm-in2023/blogapp-20@e7d17e3084f81d3ad23f6f15869dcf0d45fa9134 🚀

--- chunk-GSGK3WNW.js
+++
@@ -1,2 +0,0 @@
-import{Cb as a,Eb as o,Qa as i,Rb as n}from"./chunk-Y2R74FCU.js";var l=class t{id=n.required();static \u0275fac=function(e){return new(e||t)};static \u0275cmp=i({type:t,selectors:[["BlogDetailPage"]],inputs:{id:[1,"id"]},decls:1,vars:1,template:function(e,p){e&1&&a(0),e&2&&o("The id of the Blog: ",p.id(),`
-`)},encapsulation:2})};export{l as BlogDetailPage};
