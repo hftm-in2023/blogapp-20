a30c2b0b15c277a2d1e5ee75b9ccc274941a6ee9
Roger Rudin <roger.rudin@gmail.com>
2025-09-05 16:05:07 +0200

    fix: fix tests

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,10 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { App } from './app';
+import { MockProvider } from 'ng-mocks';
+import { AuthStore } from './core/auth';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
+      providers: [MockProvider(AuthStore)],
     }).compileComponents();
   });
 
