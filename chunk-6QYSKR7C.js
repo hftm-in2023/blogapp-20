645fa4e6a1a73b17e233e7b40657c565eba22ee0
rudini <roger.rudin@gmail.com>
2025-09-25 21:00:13 +0000

    Deploying to prod from @ hftm-in2023/blogapp-20@e1331a1fcc8395a50ebab7ea996fbf763707c1b6 🚀

--- chunk-6QYSKR7C.js
+++
@@ -1,2 +0,0 @@
-import{$b as o,bc as n,fb as i,rc as l}from"./chunk-JLEU773K.js";var t=class a{id=l.required();static \u0275fac=function(e){return new(e||a)};static \u0275cmp=i({type:a,selectors:[["BlogDetailPage"]],inputs:{id:[1,"id"]},decls:1,vars:1,template:function(e,g){e&1&&o(0),e&2&&n("The id of the Blog: ",g.id(),`
-`)},encapsulation:2,changeDetection:0})};export{t as default};
---
+++ chunk-BU3V4F3C.js
@@ -0,0 +1,2 @@
+import{$b as n,Zb as o,fb as i,pc as l}from"./chunk-77TVTEMB.js";var t=class a{id=l.required();static \u0275fac=function(e){return new(e||a)};static \u0275cmp=i({type:a,selectors:[["BlogDetailPage"]],inputs:{id:[1,"id"]},decls:1,vars:1,template:function(e,g){e&1&&o(0),e&2&&n("The id of the Blog: ",g.id(),`
+`)},encapsulation:2,changeDetection:0})};export{t as default};
