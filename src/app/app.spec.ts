9e93973bf6cacf7c57aa844beed0717a8b4f3c9e
Roger Rudin <roger.rudin@gmail.com>
2025-10-24 10:02:02 +0200

    feat: fix tests

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -3,12 +3,17 @@ import { App } from './app';
 import { MockProvider } from 'ng-mocks';
 import { AuthStore } from './core/auth';
 import { provideRouter } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
-      providers: [provideRouter([]), MockProvider(AuthStore)],
+      providers: [
+        provideRouter([]),
+        MockProvider(AuthStore),
+        MockProvider(TranslateService),
+      ],
     }).compileComponents();
   });
 
