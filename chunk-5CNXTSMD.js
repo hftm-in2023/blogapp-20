64a1b317a70bac8de61ab767f960cda7e6f6a38d
rudini <roger.rudin@gmail.com>
2026-03-13 13:28:58 +0000

    Deploying to prod from @ hftm-in2023/blogapp-20@499d747cd4d4d1a32151a1d0385f5be734798596 🚀

--- chunk-5CNXTSMD.js
+++
@@ -1,1 +0,0 @@
-var e={production:!0,serviceUrl:"https://d-cap-blog-backend---v2.whitepond-b96fee4b.westeurope.azurecontainerapps.io"};export{e as a};
---
+++ chunk-QP6EKXAJ.js
@@ -0,0 +1,1 @@
+var n={production:!0,bffUrl:"/api"};export{n as a};
